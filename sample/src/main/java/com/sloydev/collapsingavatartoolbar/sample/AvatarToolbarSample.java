b3c10b91d564e7b56187ea37c76ba8664c187b58
Rafa Vázquez <rafa.vazsan@gmail.com>
2015-08-27 21:40:12 +0200

    Added sample without library
    Created modules for library and sample.
    Library is empty for now.
    Sample contains an Activity with a nice layout, but without
    the avatar toolbar.

---
+++ sample/src/main/java/com/sloydev/collapsingavatartoolbar/sample/AvatarToolbarSample.java
@@ -0,0 +1,16 @@
+package com.sloydev.collapsingavatartoolbar.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class AvatarToolbarSample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_avatar_toolbar_sample);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_avatar_toolbar_sample);
+    }
+}
